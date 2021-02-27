2a31169855865223f9964431fc1f976ad41c96d4
Sharon <sharon.raveendran@ideas2it.com>
2021-02-28 00:11:20 +0530

    Fourth commit

--- com/ideas2it/employeemanagement/EmployeeManagement.java
+++ com/ideas2it/employeemanagement/EmployeeManagement.java
@@ -12,50 +12,38 @@ import com.ideas2it.employeemanagement.model.Employee;
 /**
  * Doing CRUD operation in collection
  * @author Sharon
- * @created 23-02-2021
+ * @created 28-02-2021
  */
 public class EmployeeManagement {
-    int id = 0;
-    Map<Integer, Employee> employees = new HashMap<Integer, Employee>();
-    static Scanner scanner = new Scanner(System.in);
-    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    public static void main(String[] args) { 
-	int flag = 0;
-	EmployeeManagement employeeManagement = new EmployeeManagement();
-	String message = "\nSelect your option\n1 : Create employee\n"
-	    	+ "2 : Read employee\n3 : Update employee\n"
-	    	+ "4 : Delete employee\n5 : Display all\n6 : Exit";
-	while (0 == flag) {
-	    System.out.println(message);
-            int option = Integer.parseInt(scanner.nextLine());
-            switch (option) {
-                case 1:
-                    employeeManagement.createEmployee();
-		    break;
-                case 2:
-		    employeeManagement.readEmployee();
-		    break;	
-	        case 3:
-		    employeeManagement.updateEmployee();
-		    break;
-	        case 4:
-		    employeeManagement.deleteEmployee();
-		    break;
-	        case 5:
-		    employeeManagement.displayAll();
-		    break;
-	        case 6:
-		    flag = 1;
-		    System.out.println("Thank you");
-	        default:
-		    System.out.println("Please enter a valid option");
-	    }	
-	}		
-    }
+	int id = 0;
+	Map<Integer, Employee> employees = new HashMap<Integer, Employee>();
+	static Scanner scanner = new Scanner(System.in);
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyy");
 	
-    /**
-     * Creating employee and storing in map
-     */
+	public static void main(String[] args) { 
+		EmployeeManagement employeeManagement = new EmployeeManagement();
+		int flag = 0;	
+		String message = "\nSelect your option\n1 : Create employee\n"
+ 	            + "2 : Read employee\n3 : Update employee\n"
+ 	    		+ "4 : Delete employee\n5 : Display all\n6 : Exit";
+		int option = 0;
+		do {
+		    do {
+		        System.out.println(message);
+		        try {
+		    	    option = Integer.parseInt(scanner.nextLine());
+		    	    flag = 1;
+    	        } catch (NumberFormatException e) {
+    		        System.out.println("Please enter a valid option");
+    	        }
+		    } while (0 == flag);
+		    employeeManagement.start(option, message, employeeManagement);
+		} while(6 != option);   			
+	}
+	
+	/**
+	 * Creating employee and storing in map
+	 */
     private void createEmployee() {
     	id++;
     	long salary = 0l;
@@ -65,27 +53,19 @@ public class EmployeeManagement {
     	System.out.println("Enter designation");
     	String designation = scanner.nextLine();
     	System.out.println("Enter salary");
-    	while (0 == flag) {  	    
+    	do {  	    
     	    try {
-    	        salary = Integer.parseInt(scanner.nextLine());
+    	        salary = Long.parseLong(scanner.nextLine());
     	        flag = 1;
     	    } catch (NumberFormatException e) {
     		    System.out.println("Please enter a valid salary");
     	    }
-    	}
+    	} while (0 == flag);
     	long mobile = createMobile();
-    	System.out.println(" Enter date in given format dd/mm/yyyy");
-        while (1 == flag) {
-	   String date =scanner.nextLine();
-	   try {
-	       Date dob =sdf.parse(date);
-	       Employee employee = new Employee(name, designation, salary, id, mobile, (sdf.format(dob)));
-	       employees.put(id, employee);
-	       flag = 3;
-	   } catch (ParseException e) {
-	       System.out.println("Enter a valid date ");
-	   }
-        }  	
+    	Date dob = createDob();
+    	Employee employee = new Employee(name, designation, salary, id, mobile, dob);
+    	employees.put(id, employee);   	
+    	System.out.println("\nEmployee created successfully...\n");
     }
     
     /**
@@ -115,81 +95,29 @@ public class EmployeeManagement {
      */
     private void updateEmployee() {
         int option;
-    	int flag1 = 0;
-    	int flag2 = 0;
-    	int flag3 = 0;
-    	int flag4 = 0;
     	System.out.println("Enter employee id"); 
-    	while(0 == flag1) {
-    		flag1 =1;
-    	    try {
-    	        id = Integer.parseInt(scanner.nextLine());
-    	        if ( null == employees.get(id)) {
-        	        System.out.println("No employee present with the given id");
-        	    } else {
-        	    	Employee employee = employees.get(id);
-        	    	String message = "What you want to update\n1 : Name\n"
-        	    			+ "2 : Designation\n3 : Salary\n4 : DOB\n5 : Mobile";
-        	    	System.out.println(message);
-        	        while (0 == flag3) {
-        	    	    try {
-        	    	        option = Integer.parseInt(scanner.nextLine());
-        	    	    } catch (NumberFormatException e) {
-        	    	        option = 4;
-        	            }
-        	            flag3 = 1;
-        	            switch (option) {
-        	    	        case 1: 
-        	    		    System.out.println("Enter new Name");
-        	    		    String newName = scanner.nextLine();
-        	    		    employee.setName(newName);
-        	    		    break;	
-        	    	        case 2: 
-        	    		    System.out.println("Enter new Designation");
-        	    		    String newDesignation = scanner.nextLine();
-        	    		    employee.setDesignation(newDesignation);
-        	    		     break;
-        	    	        case 3: 
-        	    		    System.out.println("Enter new Salary");
-        	    		    while (0 == flag2) {  	    
-        	    		        try {
-        	    		           int newSalary = Integer.parseInt(scanner.nextLine());
-        	    		            employee.setSalary(newSalary);
-        	    		            flag2 = 1;
-        	    		        } catch (NumberFormatException e) {
-        	    		            System.out.println("Please enter a valid salary");
-        	    		        }
-        	    		    }
-        	    		    break;	
-        	    	        case 4:
-        	    	            System.out.println(" Enter new date in given format dd/mm/yyyy");
-        	    	    	    while (0 == flag4) {
-        	    	    		String date =scanner.nextLine();
-        	    	    		try {
-        	    	    	            Date dob =sdf.parse(date);
-        	    	    	            employee.setDob(sdf.format(dob));
-        	    	    		    flag4 = 1;
-        	    	    		} catch (ParseException e) {
-        	    	    		    System.out.println("Enter a valid date ");
-        	    	    		}
-        	    	            }
-        	    	            break;
-        	    	        case 5:
-        	    	            System.out.println("Enter new mobile number");
-        	    	            long mobile = scanner.nextLong();
-        	    	            scanner.nextLine();
-        	    	            employee.setMobile(mobile);
-        	    	            break;
-        	    	        default:
-        	    		    System.out.println("Enter a valid input");
-        	    		    flag3 = 0;
-        	    	    }
-        	        }
-        	    }
-    	    } catch (NumberFormatException e) {
-    		    System.out.println("Enter a valid employee id");
-    		    flag1 = 0;
-    	    }   
+    	int id = isValidId() ;
+    	if (0 != id) {	
+    	    String message = "What you want to update\n1 : Name\n"
+        	        + "2 : Designation\n3 : Salary\n4 : DOB\n5 : Mobile";
+            System.out.println(message);
+    	    option = isValidOption();
+    	    switch (option) {
+    	        case 1:
+    	    	    updateName(id);
+    	    	    break;
+    	    	case 2:
+    	    	    updateDesignation(id);
+    	    	    break;
+    	    	case 3:
+    	    	    updateSalary(id);
+    	    	    break;
+    	    	case 4:
+    	    	    updateDob(id);
+    	    	    break;
+    	    	case 5:
+    	    	    updateMobile(id);
+    	    }
     	}
     }
     
@@ -218,9 +146,9 @@ public class EmployeeManagement {
     	        	System.out.println("No employee present with given id");
     	        	flag = 1;
     	        } else {
-    	        employees.remove(id);
-    	        System.out.println("Employee deleted successfully..");
-    	        flag = 1;
+    	            employees.remove(id);
+    	            System.out.println("Employee deleted successfully..");
+    	            flag = 1;
     	        }
     	    } catch (NumberFormatException e) {
     		    System.out.println("Please enter a valid employee id");
@@ -230,21 +158,190 @@ public class EmployeeManagement {
     
     /**
      * 
-     * @return return mobile number
+     * @return  mobile number
      */
     private long createMobile() {
     	long mobile = 0l;
     	int flag = 0;
     	System.out.println("Enter mobile");
-    	while (0 == flag) {  	    
+    	do{  	
     	    try {
-    	    	mobile = scanner.nextLong();
-    	    	scanner.nextLine();
-    	        flag = 2;
+    	    	mobile = Long.parseLong(scanner.nextLine());
+    	        flag = 1;
     	    } catch (Exception e) {
     		    System.out.println("Please enter a valid mobile");
     	    }
-    	}
+    	} while (flag == 0);
     	return mobile;
     }
+    
+    /**
+     * Check whether the employee id is valid or not
+     * @return  the employee id
+     */
+    private int isValidId() {int x = 0;
+    	do {
+    	    try {
+ 	            id = Integer.parseInt(scanner.nextLine());
+ 	            if ( null == employees.get(id)) {
+     	            System.out.println("No employee present with the given id");x=1;
+     	            return 0;
+     	        } else {
+     	    	    Employee employee = employees.get(id);x=1;
+                }
+    	     } catch (NumberFormatException e) {
+ 		        System.out.println("Enter a valid employee id");
+ 	         }
+    	} while(0 == x);
+        return id;
+    }
+    
+    /*
+     * This method will update the employee name
+     * @param id the employee id
+     */
+    private void updateName(int id) {
+    	Employee employee = employees.get(id);
+    	System.out.println("Enter new Name");
+        String employeeName = scanner.nextLine();
+        employee.setName(employeeName);  	
+    }
+    
+    /*
+     * This method will update the employee Designation
+     * @param id the employee id
+     */
+    private void updateDesignation(int id) {
+    	Employee employee = employees.get(id);
+    	System.out.println("Enter new Designation");
+        String newDesignation = scanner.nextLine();
+        employee.setDesignation(newDesignation);
+    }
+    
+    /*
+     * This method will update the employee Salary
+     * @param id the employee id
+     */
+    private void updateSalary(int id) {
+    	Employee employee = employees.get(id);
+    	System.out.println("Enter new Salary");
+    	int flag = 0;
+        do {  	    
+            try {
+               long newSalary = Long.parseLong(scanner.nextLine());
+               employee.setSalary(newSalary);
+               flag = 1;
+               
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid salary");
+            }
+        } while (flag == 0);
+    }
+    
+    /*
+     * This method will update the employee Date of birth
+     * @param id the employee id
+     */
+    private void updateDob(int id) {
+    	int flag = 0;
+    	Employee employee = employees.get(id);
+    	System.out.println(" Enter new date in given format dd/mm/yyyy");
+		do {
+		    String date =scanner.nextLine();
+		    try {
+			    Date dob =sdf.parse(date);
+			    employee.setDob(dob);
+			    flag = 1;
+		    } catch (ParseException e) {
+		        System.out.println("Enter a valid date ");
+		    }
+		} while (flag == 0);	
+    }
+    
+    /*
+     * This method will update the employee Mobile number
+     * @param id the employee id
+     */
+    private void updateMobile(int id) {
+        Employee employee = employees.get(id);
+        System.out.println("Enter new mobile number");
+        int flag = 0;
+        do {  	    
+            try {
+                long mobile = Long.parseLong(scanner.nextLine());
+                employee.setMobile(mobile);
+                flag = 1;              
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid mobile");
+            }
+        } while (flag == 0);
+    }
+    
+    /*
+     * This method will create the employee Date of birth
+     */
+     private Date createDob() {
+    	 int flag = 0;
+    	 Date dob = null;
+    	 String date;
+    	 System.out.println("Enter new date in given format dd/mm/yyyy");
+ 		do {
+ 		    date =scanner.nextLine();
+ 		    try {
+ 			    dob =sdf.parse(date);
+ 			    flag = 1;
+ 		    } catch (ParseException e) {
+ 		        System.out.println("Enter a valid date ");
+ 		    }
+ 		}while (flag == 0);	  
+ 		return dob;
+     }
+     
+     /*
+      * This method will validate the option
+      */
+     private int isValidOption() {
+    	 int option;
+    	 do {
+	         try {	    
+  	             option = Integer.parseInt(scanner.nextLine());
+  	             if(option>5)throw new  NumberFormatException();
+  	         } catch (NumberFormatException e) {
+  	             System.out.println("Enter a valid option");
+  	        	 option = 6;
+             }
+	     } while (option == 6);
+    	 return option;
+     }
+     
+     /*
+      * This method will update start the CRUD operation
+      * @param option user given option
+      * @param message message for user to input option
+      * @param employeeManagement EmployeeManagement class object
+      */
+     public void start(int option, String message, EmployeeManagement employeeManagement) {	
+    	 switch (option) {
+	        case 1:
+	        	employeeManagement.createEmployee();
+		        break;
+	        case 2:
+	        	employeeManagement.readEmployee();
+		        break;	
+	        case 3:
+	        	employeeManagement.updateEmployee();
+		        break;
+	        case 4:
+	        	employeeManagement.deleteEmployee();
+		        break;
+	        case 5:
+	        	employeeManagement.displayAll();
+	        	break;
+	        case 6:
+	        	System.out.println("Thank you");
+	        	break;
+	        default:
+	        	System.out.println("Please enter a valid option");
+		} 	 
+     }
 }
--- com/ideas2it/employeemanagement/model/Employee.java
+++ com/ideas2it/employeemanagement/model/Employee.java
@@ -1,82 +1,84 @@
 package com.ideas2it.employeemanagement.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * POJO class for employee providing id,
  * name, designation & salary. 
  * @author Sharon
- * @created 23-02-2021
+ * @created 28-02-2021
  */
 public class Employee {
-    private int id;
-    private String name;
+	private int id;
+	private String name;
     private String designation;
     private long salary;
     private long mobile;
-    private String dob;
+    private Date dob;
     
-    public Employee(String name, String designation, long salary, int id, long mobile, String dob) {
+    public Employee(String name, String designation, long salary, int id, long mobile, Date dob) {
     	this.name = name;
     	this.designation = designation;
     	this.salary = salary;
     	this.id = id;
     	this.mobile = mobile;
     	this.dob = dob;
-    }
+	}
 
-    public String toString() {
+	public String toString() {
     	return "\nEmployee Id          =  " + id + "\nEmployee Name        =  "
-        	+ name + "\nEmployee Designation =  " + designation
-    	        + "\nEmployee Salary      =  " + salary
-    	        +"\nMobile               =  " + mobile + "\nDate of Birth        =  " + dob + "\n";
+    			+ name + "\nEmployee Designation =  " + designation
+    			+ "\nEmployee Salary      =  " + salary
+    			+"\nMobile               =  " + mobile + "\nDate of Birth        =  "
+    			+ new SimpleDateFormat("dd/mm/yyy").format(dob) + "\n";
     }
     
     public String getName() {
 		return name;
-    }
+	}
     
-    public void setName(String name) {
+	public void setName(String name) {
 		this.name = name;
-    }
+	}
 	
-    public String getDesignation() {
+	public String getDesignation() {
 		return designation;
-    }
+	}
 	
-    public void setDesignation(String designation ) {
+	public void setDesignation(String designation ) {
 		this.designation = designation;
-    }
+	}
 	
-    public int getId() {
+	public int getId() {
 		return id;
-    }
+	}
 	
-    public void setId(int id) {
+	public void setId(int id) {
 		this.id = id;
-    }
+	}
 	
-    public long getSalary() {
+	public long getSalary() {
 		return salary;
-    }
+	}
 	
-    public void setSalary(int salary) {
+	public void setSalary(long salary) {
 		this.salary = salary;
-    } 
+	} 
 	
-    public void setMobile(long mobile) {
+	public void setMobile(long mobile) {
 		this.mobile = mobile;
-    }
+	}
 	
-    public long getMobile() {
+	public long getMobile() {
 		return mobile;
-    }
+	}
 	
-    public void setDob(String dob) {
+	public void setDob(Date dob) {
 		this.dob = dob;
-    }
+	}
 	
-    public String getDob() {
+	public Date getDob() {
 		return dob;
-    }
+	}
 }
